207fb5f8646e5582eadacb892ac4cc596d229827
zainzh_scipark <zhengsun@link.cuhk.edu.hk>
2023-03-16 16:35:16 +0800

    1

--- project/05_web_display_camera_sample_smartbin/mipi_camera_web_test.py
+++ project/05_web_display_camera_sample_smartbin/mipi_camera_web_test.py
@@ -147,7 +147,7 @@ def decode(outputs, score_threshold, origin_shape, input_size=512):
             x1 = min(max(x1, 0), 1)
             y1 = min(max(y1, 0), 1)
 
-            pred_bbox = np.concatenate((np.array(x0, y0, x1 - x0, y1 - y0), float(score), clsid),  dtype=np.float32)
+            pred_bbox = np.concatenate((np.array([x0, y0, x1 - x0, y1 - y0]), float(score), clsid), axis=1, dtype=np.float32)
             index = pred_bbox[..., 4] > score_threshold
             pred_bbox = pred_bbox[index]
             BBOXES.append(pred_bbox)
