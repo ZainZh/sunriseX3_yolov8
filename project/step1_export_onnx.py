52f26614e27781a8aa9fba69f79698bff8e2cbaa
zainzh_scipark <zhengsun@link.cuhk.edu.hk>
2023-03-17 12:38:23 +0800

    update

--- project/step1_export_onnx.py
+++ project/step1_export_onnx.py
@@ -2,7 +2,7 @@
 from ultralytics import YOLO
 
 # 载入预训练权重
-model = YOLO("yolov8n.pt")
+model = YOLO("best.pt")
 
 # 指定 opset=11 并且使用 onnx-sim 简化 ONNX
 success = model.export(format="onnx", opset=11, simplify=True)
