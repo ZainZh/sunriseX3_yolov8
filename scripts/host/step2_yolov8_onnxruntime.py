ddb86dfbe8a66f4978b3b0add940adf92cff3684
Zain <1155169521@link.cuhk.edu.hk>
2023-10-05 14:19:33 +0800

    update

--- scripts/host/step2_yolov8_onnxruntime.py
+++ scripts/host/step2_yolov8_onnxruntime.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-from yolov8 import YOLOv8ONNX
+from src.yolov8 import YOLOv8ONNX
 
 
 if __name__ == '__main__':
@@ -10,13 +10,11 @@ if __name__ == '__main__':
     yolov8_detector = YOLOv8ONNX(model_path, conf_thres=0.2, iou_thres=0.3)
 
 
-    img = cv2.imread("/home/clover/Downloads/coco128/images/train2017/test2.jpg")
+    img = cv2.imread("../../images/kite.jpg")
 
     # Detect Objects
     boxes, scores, class_ids = yolov8_detector(img)
     # Draw detections
     combined_img = yolov8_detector.draw_detections(img)
-    cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
-    cv2.imshow("Detected Objects", combined_img)
-    # cv2.imwrite("/home/clover/Downloads/detected_objects.jpg", combined_img)
-    cv2.waitKey(0)
+    cv2.imwrite("../../images/onnx_detected.jpg", combined_img)
+    # cv2.waitKey(0)
